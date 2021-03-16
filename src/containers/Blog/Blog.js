acac97e47b8a47c982ab3d69531b4343dcdf6a7c
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-03-16 10:17:16 +0530

    http request maid

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,30 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        posts: []
+    }
+   
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then( response => {
+                    this.setState({posts: response.data});
+                   //console.log(response); 
+                } );
+    }
+    
+    render () {       
+
+        const Posts = this.state.posts.map( posts => 
+            {
+               return ( <Post key={posts.id} title={posts.title} />)
+        } );
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                   {Posts}
                 </section>
                 <section>
                     <FullPost />

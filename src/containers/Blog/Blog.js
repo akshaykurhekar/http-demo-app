d022638391234b429bb300eccd2f33a6766ca9bd
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-03-21 19:12:39 +0530

    route redirect

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost'); 
+})
 
 class Blog extends Component {
+
+    state={
+        auth: true
+    }
     render () {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink to="/posts" 
+                                 exact
+                                 activeClassName="active">Posts</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Switch>
+                    { this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    <Route path="/posts" component={Posts} />
+                    <Route render={ () => <h1>404 Error Page not Found...</h1> } />
+                    {/* <Redirect from="/" to="/posts" /> */}
+                </Switch>
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import axios from '../../../axios';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import FullPost from "../FullPost/FullPost";
 
 class Posts extends Component {
     state = {
@@ -30,25 +32,33 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+       // this.setState({selectedPostId: id});
+       this.props.history.push({pathname:'/posts/' + id})
     }
     
     render () {
         let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
+                return (
+                  //  <Link  to={'/posts' + post.id}>
+                        <Post 
+                        key={post.id}
+                        title={post.title} 
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />
+                   // </Link>
+                   );
             });
         }
 
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + '/:id'}  exact component={FullPost} />
+            </div>
         );
     }
 }
